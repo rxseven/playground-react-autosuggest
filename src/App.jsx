a060e3819ae80169278f021270b7236f6a5209c1
ME (Mac mini) <rxseven.com@gmail.com>
2018-12-18 15:36:37 +0700

    Render AutosuggestCustom component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@
 import React from 'react';
 
 // Components
+import AutosuggestCustom from './components/AutosuggestCustom';
 import Countries from './components/Countries';
 
 import './App.css';
 
 const App = () => (
   <div>
+    <h2>Custom</h2>
+    <AutosuggestCustom />
     <h2>Countries</h2>
     <Countries />
   </div>
